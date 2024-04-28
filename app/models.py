0ce1109ccf9d3b6d8615fb52124d1025d6120beb
musti <mustafaahsan2002@gmail.com>
2024-04-28 17:50:19 +0500

    first commit

---
+++ app/models.py
@@ -0,0 +1,25 @@
+from app import db, login
+from flask_login import UserMixin
+from werkzeug.security import generate_password_hash, check_password_hash
+
+@login.user_loader
+def load_user(id):
+    """Load user by their ID"""
+    return User.query.get(int(id))
+
+class User(db.Model, UserMixin):
+    """User table"""
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(512), unique=True, index=True)
+    password_hash = db.Column(db.String(512))
+
+    def __repr__(self):
+        return f'User: {self.username}'
+
+    def set_password(self, password):
+        """Hash user password before storage"""
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        """Confirms a user's password"""
+        return check_password_hash(self.password_hash, password)
---
+++ main.py
@@ -0,0 +1,9 @@
+# main.py
+from app import app
+from app.models import User
+from app import db
+@app.shell_context_processor
+
+def make_shell_context():
+
+    return dict(db=db, User=User) 
